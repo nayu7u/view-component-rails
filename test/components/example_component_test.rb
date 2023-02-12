e5568a9030a797b62dbe1f79a0359c958df4b65f
nayu <yusuke.nadaka@gmail.com>
2023-02-12 21:03:16 +0900

    test: fix sample test

--- test/components/example_component_test.rb
+++ test/components/example_component_test.rb
@@ -4,9 +4,10 @@ require "test_helper"
 
 class ExampleComponentTest < ViewComponent::TestCase
   def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(ExampleComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+    assert_equal(
+      %(<span title="my_title">Hello, components!</span>),
+      render_inline(ExampleComponent.new(title: "my_title")) { "Hello, components!" }
+                   .css("span").to_html
+    )
   end
 end
