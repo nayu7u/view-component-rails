0e54d0193b7a49b680b85c3fd83761f04208e40a
nayu <19748682+nayu7u@users.noreply.github.com>
2023-02-19 21:03:27 +0900

    style: bundle exec standardrb --fix

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -17,7 +17,7 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
 
   test "should create task" do
     assert_difference("Task.count") do
-      post tasks_url, params: { task: { description: @task.description, done: @task.done } }
+      post tasks_url, params: {task: {description: @task.description, done: @task.done}}
     end
 
     assert_redirected_to task_url(Task.last)
@@ -34,7 +34,7 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update task" do
-    patch task_url(@task), params: { task: { description: @task.description, done: @task.done } }
+    patch task_url(@task), params: {task: {description: @task.description, done: @task.done}}
     assert_redirected_to task_url(@task)
   end
 
