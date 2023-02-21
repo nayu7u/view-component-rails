10f53f7dc934659a248348cbb113a1b92a8e79d4
nayu <19748682+nayu7u@users.noreply.github.com>
2023-02-22 01:34:56 +0900

    learn: collection counter

--- app/components/product_component.rb
+++ app/components/product_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class ProductComponent < ViewComponent::Base
-  def initialize(product:)
+  def initialize(product:, product_counter:)
     @product = product
+    @product_counter = product_counter
   end
 end
