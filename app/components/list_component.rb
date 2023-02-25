3f2375679abe5887525a5a60178bed6f0ba02ccf
nayu <19748682+nayu7u@users.noreply.github.com>
2023-02-25 17:51:51 +0900

    style: bundle exec standardrb --fix

--- app/components/list_component.rb
+++ app/components/list_component.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class ListComponent < ViewComponent::Base
-
 end
--- app/components/round_button_component.rb
+++ app/components/round_button_component.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class RoundButtonComponent < ViewComponent::Base
-
 end
